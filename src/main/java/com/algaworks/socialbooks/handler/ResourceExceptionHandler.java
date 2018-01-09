ee23c76e82856d243e4644fbe12e97155811939e
Mario Junior <juniorm3@msn.com>
2018-01-09 11:27:09 -0200

    Incluido tratamento para autor

--- src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.algaworks.socialbooks.domain.DetalhesErro;
+import com.algaworks.socialbooks.services.exceptions.AutorExistenteException;
+import com.algaworks.socialbooks.services.exceptions.AutorNaoEncontradoException;
 import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
 
 @ControllerAdvice
@@ -20,6 +22,32 @@ public class ResourceExceptionHandler {
 		DetalhesErro erro  = new DetalhesErro();
 		erro.setStatus(404l);
 		erro.setTitulo("O Livro não pode ser encontrado.");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
+		erro.setTitestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(AutorExistenteException.class)
+	public ResponseEntity<DetalhesErro> handleAutorExistenteException
+	(AutorExistenteException e,	HttpServletRequest request) {
+		
+		DetalhesErro erro  = new DetalhesErro();
+		erro.setStatus(409l);
+		erro.setTitulo("Autor já existente.");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/409");
+		erro.setTitestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+	}
+	
+	@ExceptionHandler(AutorNaoEncontradoException.class)
+	public ResponseEntity<DetalhesErro> handleAutorNaoEncontradoException
+			(AutorNaoEncontradoException e,	HttpServletRequest request) {
+		
+		DetalhesErro erro  = new DetalhesErro();
+		erro.setStatus(404l);
+		erro.setTitulo("O autor não pode ser encontrado.");
 		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
 		erro.setTitestamp(System.currentTimeMillis());
 		
